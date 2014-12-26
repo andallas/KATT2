2651f57ae806d3b21ddf9d5ff9b216e8d4df653a
Justin <justin.35f@gmail.com>
2014-12-25 21:17:23 -0800

    Added parallax backgrounds.
    Different layers now scroll independently, and can be looped.

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scroll : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector2 direction = new Vector2(-1, 0);
+    public bool isLinkedToCamera = false;
+    public bool isLooping = false;
+
+    private List<Transform> backgroundPart;
+
+    void Start()
+    {
+        if (isLooping)
+        {
+            backgroundPart = new List<Transform>();
+ 
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+
+                if(child.renderer != null)
+                {
+                    backgroundPart.Add(child);
+                }
+            }
+
+            backgroundPart = backgroundPart.OrderBy(t => t.position.x).ToList();
+        }
+    }
+
+    void Update()
+    {
+        Vector2 movement = new Vector2(speed * direction.x, speed * direction.y);
+        movement = Vector2.ClampMagnitude(movement, speed);
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+
+        if (isLinkedToCamera)
+        {
+            Camera.main.transform.Translate(movement);
+        }
+
+        if (isLooping)
+        {
+            Transform firstChild = backgroundPart.FirstOrDefault();
+
+            if (firstChild != null &&
+                firstChild.position.x < Camera.main.transform.position.x &&
+                !firstChild.renderer.IsVisibleFrom(Camera.main))
+            {
+                Transform lastChild = backgroundPart.LastOrDefault();
+                Vector3 lastPosition = lastChild.transform.position;
+                Vector3 lastSize = (lastChild.renderer.bounds.max - lastChild.renderer.bounds.min);
+
+                firstChild.position = new Vector3(lastPosition.x + lastSize.x, firstChild.position.y, firstChild.position.z);
+
+                backgroundPart.Remove(firstChild);
+                backgroundPart.Add(firstChild);
+            }
+        }
+    }
+}
