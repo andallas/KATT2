7feb5d9cdc2b8a6b20db4c19e62368475bd96508
Justin <justin.35f@gmail.com>
2014-12-31 12:53:17 -0800

    Created bounds zones to destroy bullets.
    Bounds zones are triggers attached to a child of the camera. Any objects
    with a 'Beam' component are destroyed upon entering. This will need to
    change to be any objects with a 'Projectile' class once it's implemented,
    as well as recycle objects into an object pool rather than destroy them.

--- Assets/Scripts/Beam.cs
+++ Assets/Scripts/Beam.cs
@@ -4,9 +4,4 @@ public class Beam : MonoBehaviour
 {
     public int damage;
     public bool isEnemyShot;
-
-    void Start()
-    {
-        Destroy(gameObject, 20);
-    }
 }
---
+++ Assets/Scripts/BoundsZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BoundsZone : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Beam beam = other.gameObject.GetComponent<Beam>();
+        if(beam != null)
+        {
+            Destroy(beam.gameObject);
+        }
+    }
+}
