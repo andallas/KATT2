bdcba46544d89a13ba038c0b235b2b72d64c729b
Justin <justin.35f@gmail.com>
2015-01-07 23:04:11 -0800

    Added an enemy wave generator.

---
+++ Assets/Scripts/Enemies/WaveAction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class WaveAction
+{
+    public string name;
+    public string message;
+    public float delay;
+    public int numToSpawn;
+    public GameObject enemyPrefab;
+}
